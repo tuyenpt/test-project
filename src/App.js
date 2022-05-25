d12bdf763ccfaec79cce86e1be1f1387dcc61b95
pham trung tuyen <tuyen.pham@euroland.com>
2022-05-25 17:08:22 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      news: []
+    }
+  };
+
+  componentDidMount() {
+    axios.get('/api/news')
+         .then(res => {
+            const news = res.data;
+            this.setState({ news: news.news });
+          })
+         .catch(error => console.log(error));
+  };
+
+  render() {
+    return(
+      <ul>
+        {this.state.news.map(item => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <div>{item.description}</div>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+};
 
 export default App;
